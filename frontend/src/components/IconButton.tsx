d40ae02c080c9009b54793b7b141ecabdbf41606
Mathias Ramilo <mathiramilo2290@gmail.com>
2023-10-06 21:30:46 -0300

    Movie card zoom

--- frontend/src/components/IconButton.tsx
+++ frontend/src/components/IconButton.tsx
@@ -12,16 +12,14 @@ interface IconButtonProps {
 export default function IconButton({ Icon, text, iconSize, textSize, onClick, className }: IconButtonProps) {
   return (
     <button
-      className={`group flex items-center gap-2 ${className}`}
+      className={`flex items-center gap-2 text-white/90 hover:text-white transition-colors ${className}`}
       onClick={onClick}
     >
       <Icon
         size={iconSize || 22}
-        className="text-white/90 group-hover:text-white transition-colors"
+        className=""
       />
-      <span className={`text-${textSize || 'sm'} font-bold text-white/90 group-hover:text-white transition-colors`}>
-        {text}
-      </span>
+      <span className={`text-${textSize || 'sm'} font-bold`}>{text}</span>
     </button>
   )
 }
